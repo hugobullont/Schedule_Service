bcdef1d13360af62d56e3572eee58ca2eb7fc86a
hugobullont <hugobullont@gmail.com>
2018-09-22 19:32:49 -0500

    Eliminados comentarios

--- app/controllers/curso.controller.js
+++ app/controllers/curso.controller.js
@@ -10,10 +10,7 @@ exports.create = (req, res) => {
     const curso = new Curso({
         nombre: req.body.nombre,
         profesor: req.body.profesor,
-        faltasRestantes: req.body.faltasRestantes/*,
-        trabajos: req.body.trabajos,
-        examenes: req.body.examenes,
-        clases: req.body.clases*/
+        faltasRestantes: req.body.faltasRestantes
     });
 
     const trabajosArray = req.body.trabajos;
@@ -73,56 +70,3 @@ exports.findAll = (req, res) => {
         });
     });
 };
-/*
-// Retrieve and return all clases from the database.
-exports.findPerDay = (req, res) => {
-    const day = req.params.day
-    Clase.find({dia: day})
-    .then(clase => {
-        if(!clase) {
-            return res.status(404).send({
-                message: "Clase not found in day " + req.params.day
-            });            
-        }
-        res.send(clase);
-    }).catch(err => {
-        if(err.kind === 'ObjectId') {
-            return res.status(404).send({
-                message: "Clase not found in day " + req.params.day
-            });                
-        }
-        return res.status(500).send({
-            message: "Clase not found in day " + req.params.day
-        });
-    });
-};
-
-
-exports.findNextClass = (req, res) => {
-    const day = req.params.day
-    const hour = req.params.hour
-
-    Clase.find({dia: day})
-    .then(clases => {
-        for(var i = 0; i < clases.length; i++){
-            if((hour > clases[i].horaInicio)&&(hour<clases[i].horaFinal-15)){
-                return res.send(clases[i]);
-            }
-        }
-        var claseActual = null;
-        var hourAux = 2400; 
-        
-        for(var i = 0; i < clases.length; i++){
-            if((hour<clases[i].horaInicio)&&(clases[i].horaInicio<hourAux)){
-                claseActual = clases[i];
-                hourAux = clases[i].horaInicio;
-            }
-        }
-
-        if(claseActual === null){
-            return res.send({message: "No hay Clases"});
-        }
-        return res.send(claseActual);
-    });
-    
-}; */

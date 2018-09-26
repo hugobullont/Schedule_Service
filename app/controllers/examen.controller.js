1b122b019bf16c861758d7ac4b6969d50a165649
hugobullont <hugobullont@gmail.com>
2018-09-25 22:24:31 -0500

    Updates

--- app/controllers/examen.controller.js
+++ app/controllers/examen.controller.js
@@ -4,9 +4,7 @@ const Examen = require('../models/examen.models.js');
  //GET Examen cercano para un curso via ID
  exports.findPerDateAndID = (req,res) => {
     const id = req.params.cursoId;
-    console.log(id);
     const date = req.params.date;
-    console.log(date);
     var todos = [];
     var minorDate = 20501231;
     var examenCercano;
@@ -14,7 +12,6 @@ const Examen = require('../models/examen.models.js');
     Curso.findOne({'_id': id})
     .then(curso =>{
         todos = curso.examenes;
-        console.log(todos);
         for(var i = 0; i < todos.length; i++){
             if((todos[i].fecha >= date) && (todos[i].fecha < minorDate)){
                 minorDate = todos[i].fecha;
--- app/controllers/trabajo.controller.js
+++ app/controllers/trabajo.controller.js
@@ -4,9 +4,7 @@ const Trabajo = require('../models/trabajo.models.js');
  //GET Trabajo cercano para un curso via ID
  exports.findPerDateAndID = (req,res) => {
     const id = req.params.cursoId;
-    console.log(id);
     const date = req.params.date;
-    console.log(date);
     var todos = [];
     var minorDate = 20501231;
     var trabajoCercano;
@@ -14,7 +12,6 @@ const Trabajo = require('../models/trabajo.models.js');
     Curso.findOne({'_id': id})
     .then(curso =>{
         todos = curso.trabajos;
-        console.log(todos);
         for(var i = 0; i < todos.length; i++){
             if((todos[i].fechaDeEntrega >= date) && (todos[i].fechaDeEntrega < minorDate)){
                 minorDate = todos[i].fechaDeEntrega;

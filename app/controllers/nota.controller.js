d4ef6d2638a68d7bfa81f06e07defbef22a556b2
hugobullont <hugobullont@gmail.com>
2018-10-06 07:38:14 -0500

    Added notasAcumuladas

--- app/controllers/nota.controller.js
+++ app/controllers/nota.controller.js
@@ -11,3 +11,28 @@ exports.findAllForID = (req,res) => {
         return res.send(curso.notas);
     });
 };
+
+//GET Todas las notas por curso acumuladas.
+exports.totalNotasAcum = (req,res) => {
+    console.log("GET /notasAcumuladas");
+    var objects = []
+    Curso.find()
+    .then(cursos =>{
+        for(var i = 0; i < cursos.length; i++){
+            var notas = cursos[i].notas;
+            var notaAcumulada = 0;
+            var porcentaje = 0;
+            for(var j = 0; j < notas.length; j++){
+                var percent = notas[i].porcentaje / 100;
+                notaAcumulada += notas[i].valor*percent;
+                porcentaje += notas[i].porcentaje;
+            }
+            var object = new Object();
+            object.nombreCurso = curso[i].nombre;
+            object.notaAcumulada = notaAcumulada;
+            object.porcentaje = porcentaje;
+            objects.push(object);
+        }
+        return res.send(objects);
+    });
+};
--- app/routes/nota.routes.js
+++ app/routes/nota.routes.js
@@ -1,4 +1,5 @@
 module.exports = (app) => {
     const notas = require('../controllers/nota.controller.js');
     app.get('/notas/:cursoId',notas.findAllForID);
+    app.get('/notasAcumuladas',notas.totalNotasAcum);
 }

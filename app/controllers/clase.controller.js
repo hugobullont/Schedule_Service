8dd2210327d21b26e9dc13a194f9f8adc7c881eb
hugobullont <hugobullont@gmail.com>
2018-10-20 14:27:57 -0500

    message

--- app/controllers/clase.controller.js
+++ app/controllers/clase.controller.js
@@ -101,33 +101,38 @@ exports.findNextClassesPerDay = (req, res) => {
         }
         var final = clasesPendientes.length;
         var contador = 0;
-        var ids = []
+        var ids = [];
         for (var i = 0; i < clasesPendientes.length; i++){
             ids.push(clasesPendientes[i]._id);
         }
+
+        var cursos = [];
         console.log(ids);
         for (var i = 0; i < clasesPendientes.length; i++){
             console.log(clasesPendientes[i]._id);
             Curso.findOne({'clases._id':  ids[i]})
             .then(curso =>{
-                console.log(curso.nombre);
-                var object = new Object();
-                var cursoFinal = new Object();
-                cursoFinal._id = curso._id;
-                cursoFinal.nombre = curso.nombre;
-                cursoFinal.profesor = curso.profesor;
-                cursoFinal.faltasRestantes = curso.faltasRestantes;
-                object.Curso = cursoFinal;
-                object.Clase = clasesPendientes[contador];
-                respuesta.push(object);
-                console.log('Added');
-                if (contador == clasesPendientes.length - 1){
-                    return res.send(respuesta);
-                }
-                contador = contador + 1;
-                object = new Object();
-                cursoFinal = new Object();
+                cursos.push(curso);
             });
         }
+
+        for (var i = 0; i < clasesPendientes.length; i++){
+            var object = new Object();
+            var cursoFinal = new Object();
+            cursoFinal._id = cursos[i]._id;
+            cursoFinal.nombre = cursos[i].nombre,
+            cursoFinal.profesor = cursos[i].profesor;
+            cursoFinal.faltasRestantes = cursos[i].faltasRestantes;
+            object.Curso = cursoFinal;
+            object.Clase = clasesPendientes[i];
+            respuesta.push(object);
+            console.log('Added');
+            if (i == clasesPendientes.length - 1){
+                return res.send(respuesta);
+            }
+        }
+
+        
+                
     });
 };

2155d26dd59947b46587a1d719e79adb0eca5329
hugobullont <hugobullont@gmail.com>
2018-10-20 14:06:19 -0500

    message

--- app/controllers/clase.controller.js
+++ app/controllers/clase.controller.js
@@ -1,6 +1,15 @@
 const Clase = require('../models/clase.models.js');
 const Curso = require('../models/node.models.js');
 
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
+
 // Retrieve and return all clases from the database.
 exports.findPerDay = (req, res) => {
     const day = req.params.day
@@ -122,6 +131,7 @@ exports.findNextClassesPerDay = (req, res) => {
                 object = new Object();
                 cursoFinal = new Object();
             });
+            sleep(1000);
         }
     });
 };
